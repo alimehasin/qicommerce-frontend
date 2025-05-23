c665eea686ad556354fb027d4dc94621b840329e
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-05-23 17:46:13 +0300

    feat: update environment configuration to include storage URL and enhance image handling in Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import { env } from "@/env";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL(`${env.NEXT_PUBLIC_STORAGE_BASE_URL}/**`)],
+  },
 };
 
 export default nextConfig;
